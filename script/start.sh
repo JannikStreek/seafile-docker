818e462ca455103fbed80e9dc7ae194272fc07ca
JannikStreek <theshowcanbegin@gmail.com>
2017-08-19 12:42:47 +0200

    Update start.sh

--- script/start.sh
+++ script/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 . /usr/local/bin/seafile_env.sh
 
+log_info $LATEST_SERVER_DIR
+
 EXPOSED_DIRS="conf ccnet logs seafile-data seahub-data"
 EXPOSED_ROOT_DIR=${EXPOSED_ROOT_DIR:-"/seafile"}
 
